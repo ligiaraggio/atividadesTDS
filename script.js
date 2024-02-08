c179a25caf9227418247cbe1949cab1b67304682
ligiaraggio <143438477+ligiaraggio@users.noreply.github.com>
2024-02-08 08:50:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var nomeGlobal;
+var mensagemGlobal;
+
+function conferirMensagemWhatsapp(){
+    var nome = document.getElementById("nome").value;
+    var mensagem = document.getElementById("mensagem").value;
+    
+    nomeGlobal = nome;
+    mensagemGlobal = mensagem;
+
+    document.getElementById("confNome").textContent = nome;
+    document.getElementById("confMsg").textContent = mensagem;
+    
+}
+
+    function enviar(){
+        var numeroTelefone = "5541999999999"
+        
+        var linkWhatsApp = "https://wa.me/" + 
+        numeroTelefone + 
+        "?text=Nome:" + 
+        nomeGlobal + 
+        " - " + 
+        mensagemGlobal;
+
+        window.open(linkWhatsApp, "_blank")
+
+    }
